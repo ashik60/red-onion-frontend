ddc35b3905bc5e839f22652ad11498190dee3fbd
ashiksh <ashikshaheen187@gmail.com>
2021-04-30 16:27:36 +0600

    Food area

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Banner from './components/Banner/Banner';
+import Foods from './components/Foods/Foods';
 import Header from './components/Header/Header';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
                 <Route path='/'>
                     <Header />
                     <Banner />
-                  
+                    <Foods />
                 </Route>
             </Switch>
         </Router>
--- src/components/Foods/Foods.js
+++ src/components/Foods/Foods.js
@@ -1,20 +1,50 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import FoodItem from '../FoodItem/FoodItem';
+import './Foods.css';
 
-const Foods = () => {
+const Foods = (props) => {
     const [foods, setFoods] = useState([]);
     const [selectedFoodType, setSelectedFoodType] = useState('Breakfast');
 
     useEffect(() => {
-        axios
-            .get('https://thawing-temple-99876.herokuapp.com/foods')
+        fetch('https://thawing-temple-99876.herokuapp.com/foods')
             .then((res) => res.json())
             .then((data) => setFoods(data));
     }, []);
 
     const selectedFoods = foods.filter((food) => food.type === selectedFoodType);
 
-    return <div></div>;
+    return (
+        <section className='food-area my-5'>
+            <div className='container'>
+                <nav>
+                    <ul className='nav justify-content-center'>
+                        <li className='nav-item' onClick={() => setSelectedFoodType('Breakfast')}>
+                            <span className={selectedFoodType === 'Breakfast' ? 'active nav-link' : 'nav-link'}>Breakfast</span>
+                        </li>
+                        <li className='nav-item' onClick={() => setSelectedFoodType('Lunch')}>
+                            <span className={selectedFoodType === 'Lunch' ? 'active nav-link' : 'nav-link'}>Lunch</span>
+                        </li>
+                        <li className='nav-item' onClick={() => setSelectedFoodType('Dinner')}>
+                            <span className={selectedFoodType === 'Dinner' ? 'active nav-link' : 'nav-link'}>Dinner</span>
+                        </li>
+                    </ul>
+                </nav>
+                <div className='row my-5'>
+                    {selectedFoods.map((food) => (
+                        <FoodItem food={food} key={food.id} />
+                    ))}
+                </div>
+                <div className='text-center'>
+                    {/* {props.cart.length && (
+                    <Link to='/checkout'>
+                        <button className='btn btn-secondary'>Check Out Your Food</button>
+                    </Link>
+                )} */}
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Foods;
